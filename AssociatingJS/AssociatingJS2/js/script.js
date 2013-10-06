a73111d2d85fc224da80fe308a460593317ffd7a
Elimarie <iglitzi@fullsail.edu>
2013-10-05 21:52:22 -0400

    Video: Values and Variables Lesson

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,2 +1,32 @@
-alert("test");
+
+var yearBorn =1975;
+
+/*
+
+console.log("Hello World!");
+console.log(yearBorn);
+
+    */
+
+alert(yearBorn);
+
+var age = 39;
+
+var isStudent = false;
+
+var firstName = "Elimarie";
+
+var phrase = 'I dont\'t know \n You\'ll have to speak with the professor'; //0,1,2,3
+
+
+var muppetsName = ["Kermit", "MissPiggy", "Gonzo", "Rizzo"];
+
+var num = 1
+
+console.log(muppetsName[num]);
+
+
+
+
+
 
