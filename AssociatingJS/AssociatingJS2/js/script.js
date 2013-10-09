9d2b9542cece8bf1b61098a49870ab2105901ca4
Elimarie <iglitzi@fullsail.edu>
2013-10-08 21:28:31 -0400

    Activity Slice of of Pie part II

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,17 +1,20 @@
 // Elimarie Morales Santiago
-// October 7, 2013
+// October 8, 2013
 // Activity: Expression Worksheet
 
-// Slice of of Pie part 1
+// Slice of of Pie part II
 
 
 var slicesOfPizza= 8; // Each pizza has 8 slices
 var persons= 23;   // I have 23 persons at the party
 var pizzasOrdered= 5; // Total of pizzas ordered
 
-var sliceperperson= slicesOfPizza*pizzasOrdered/persons; // Total is 1.73 slice of pizza per person
 
-console.log(sliceperperson);
+var sliceperperson= slicesOfPizza*pizzasOrdered/persons; // Total is 1.73 slices of pizza per person
+
+var pizzaForSparky= slicesOfPizza*pizzasOrdered-persons; // Sparky will eat 17 pizzas
+
+console.log(pizzaForSparky);
 
 
 
