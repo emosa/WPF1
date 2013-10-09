cadfc54d80c0973e095f120db37467750afe88ef
Elimarie <iglitzi@fullsail.edu>
2013-10-08 21:16:45 -0400

    Activity Slice of Pie part 1

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -2,29 +2,18 @@
 // October 7, 2013
 // Activity: Expression Worksheet
 
-// Dog Years
-//For each human year sparky ages 7 years
-//var sparkyAge = 1;
-//var humanYears = 7;
-
-//var numberOfYears=2;
-//var sparkyAge=numberOfYears*7;
-
-//var resuthy = sparkyAge * humanYears;
-
-//var result = "Sparky is"  + " " + numberOfYears + " " + "human years old which is " + " " + sparkyAge + " " + "dog years.";
-
-//console.log(result);
+// Slice of of Pie part 1
 
 
-//You have a bag of marbles. In the bag there are an un-known number of marbles. But you know there are 30% red, 30% blue 40% green
-//if you have 6 red and 6 blue how many green do you have?
+var slicesOfPizza= 8; // Each pizza has 8 slices
+var persons= 23;   // I have 23 persons at the party
+var pizzasOrdered= 5; // Total of pizzas ordered
+
+var sliceperperson= slicesOfPizza*pizzasOrdered/persons; // Total is 1.73 slice of pizza per person
+
+console.log(sliceperperson);
 
 
-var green= 40; //There are %40 percent green marbles
-var totalgreen=green*.4; //I multiplied 40 * .4 then I have a total of 16 green marbles.
-
-console.log(totalgreen);
 
 
 
