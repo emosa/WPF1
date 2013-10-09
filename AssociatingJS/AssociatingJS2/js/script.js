97805b24229efcd696ec99cc6497a045cf8988fa
Elimarie <iglitzi@fullsail.edu>
2013-10-08 22:06:05 -0400

    Activity Average Shopping Bill

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -2,19 +2,16 @@
 // October 8, 2013
 // Activity: Expression Worksheet
 
-// Slice of of Pie part II
+// Average Shopping Bill
 
+var groceryWeekTotal= [125, 175, 150, 184, 132]; //  Arrays of weekly totals
+var totalspent= groceryWeekTotal[0]+groceryWeekTotal[1]+groceryWeekTotal[2]+groceryWeekTotal[3]+groceryWeekTotal[4]; //totalspend weekly $766
 
-var slicesOfPizza= 8; // Each pizza has 8 slices
-var persons= 23;   // I have 23 persons at the party
-var pizzasOrdered= 5; // Total of pizzas ordered
+var averageWeekly=totalspent/5; //average spent will be $153.20
 
+var result= "You have spent a total of $" + totalspent + " " + "on groceries over 5 weeks." + " " + "That is an average of $" + averageWeekly + " " + "per week."
 
-var sliceperperson= slicesOfPizza*pizzasOrdered/persons; // Total is 1.73 slices of pizza per person
-
-var pizzaForSparky= slicesOfPizza*pizzasOrdered-persons; // Sparky will eat 17 pizzas
-
-console.log(pizzaForSparky);
+console.log(result);
 
 
 
