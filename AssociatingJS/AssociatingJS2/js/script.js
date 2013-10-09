6640cbc2351d644bba362a6f6c29252c8ebaa7e1
Elimarie <iglitzi@fullsail.edu>
2013-10-08 20:42:48 -0400

    Activity expressions problem Dog Year and Marbles

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,9 +1,32 @@
-//Expression Assignment Operators
+// Elimarie Morales Santiago
+// October 7, 2013
+// Activity: Expression Worksheet
 
-var a = 3;
-a --;
+// Dog Years
+//For each human year sparky ages 7 years
+//var sparkyAge = 1;
+//var humanYears = 7;
 
-console.log(a);
+//var numberOfYears=2;
+//var sparkyAge=numberOfYears*7;
+
+//var resuthy = sparkyAge * humanYears;
+
+//var result = "Sparky is"  + " " + numberOfYears + " " + "human years old which is " + " " + sparkyAge + " " + "dog years.";
+
+//console.log(result);
+
+
+//You have a bag of marbles. In the bag there are an un-known number of marbles. But you know there are 30% red, 30% blue 40% green
+//if you have 6 red and 6 blue how many green do you have?
+
+
+var green= 40; //There are %40 percent green marbles
+var totalgreen=green*.4; //I multiplied 40 * .4 then I have a total of 16 green marbles.
+
+console.log(totalgreen);
+
+
 
 
 
