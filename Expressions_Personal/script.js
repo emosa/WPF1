39027598a4cf06ffab719132c50a7b5eab7c0c2d
Elimarie <iglitzi@fullsail.edu>
2013-10-09 21:35:47 -0400

    Assignment Expressions Personal fixed files

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -9,10 +9,11 @@ var depthInches= 28; // Distance from front to back of the fish tank
 var widhtInches= 14; // Distance across the front to back of the fish tank
 var heightInches= 18; // Distance from top to bottom of the fish tank
 
-var total= depthInches * widhtInches * heightInches; // Multiplying total measurements
+var total= depthInches * widhtInches * heightInches; // Multiplying total measurements for a total of 7056
 
 var gallonsub= total * 0.00433; // Calculating total measurements multiplied by cubic inches 0.00433 equal to 30.55248 that is approximately 30.55 gallons
 
 var watergallon= "My fish tank holds" +" "+ gallonsub + " " + "gallons of water."; // This is the result of all calculations
 
 console.log(watergallon); // My fish tank holds 30.55248 gallons of water.
+
