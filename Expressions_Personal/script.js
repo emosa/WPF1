f4dc7f4c744f6a4c51f0d7a1b11f6aadcd19894f
Elimarie <iglitzi@fullsail.edu>
2013-10-09 21:11:46 -0400

    Assignment Personal Expressions; How much water my fish tank holds? with added comments

--- Expressions_Personal/script.js
+++ Expressions_Personal/script.js
@@ -9,9 +9,9 @@ var depthInches= 28; //distance from front to back
 var widhtInches= 14; // Distance across the front
 var heightInches= 18; // Distance from top to bottom
 
-var total= depthInches * widhtInches * heightInches;
+var total= depthInches * widhtInches * heightInches; // Multiplying total measurements
 
-var gallonsub= total * 0.00433;
+var gallonsub= total * 0.00433; // Calculating total measurements multiplied by cubic inches 0.00433 equal to 30.55248 that is approximately 30.55 gallons
 
 
 var watergallon= "My fish tank holds" +" "+ gallonsub + " " + "gallons of water.";
