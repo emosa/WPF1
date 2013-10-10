30819d990e7cdde74643f7be31189f090c222cf7
Elimarie <iglitzi@fullsail.edu>
2013-10-10 19:43:01 -0400

    variable for user input additional comments

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,3 +7,4 @@
 var salute= alert("Welcome to My Shop! \nRate us and earn a discount!"); // variable for welcome alert
 var userName= prompt("Please enter your first and last name"); // variable for user input
 var enterNumber=prompt("On a 5 star rating, how would you rate our service? '\n(5 star being the most enjoyable experience,\nand 1 star the least.)");// variable for user input rating from 1 to 5 star
+var comments= prompt("Any additional comments would be greatly appreciated:"); 
