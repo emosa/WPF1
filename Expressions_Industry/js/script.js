72a3d64626f9046c63e7ffd3c2b3ccf3e8878d08
Elimarie <iglitzi@fullsail.edu>
2013-10-10 19:47:35 -0400

    console log with string concatenation

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,3 +9,5 @@ var userName= prompt("Please enter your first and last name"); // variable for u
 var enterNumber=prompt("On a 5 star rating, how would you rate our service? '\n(5 star being the most enjoyable experience,\nand 1 star the least.)");// variable for user input rating from 1 to 5 star
 var comments= prompt("Any additional comments would be greatly appreciated:"); // variable prompt user input additional comments
 var valueInput= alert("Thank you for your feedback! \nYou have just earned 25% Off discount! \nYour coupon code is '('MYSTAR2013')"); //variable for coupon code earned
+
+console.log("Customer Name:"+ " " +userName + " " + "Rating Number:" + " " + enterNumber + " " + "User Comments:" + " " + comments);
