cbbd2663d3cfd663473a027e284e41415107a9d4
Elimarie <iglitzi@fullsail.edu>
2013-10-10 19:46:43 -0400

    added comment for thank you and coupon code

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -8,4 +8,4 @@ var salute= alert("Welcome to My Shop! \nRate us and earn a discount!"); // vari
 var userName= prompt("Please enter your first and last name"); // variable for user input
 var enterNumber=prompt("On a 5 star rating, how would you rate our service? '\n(5 star being the most enjoyable experience,\nand 1 star the least.)");// variable for user input rating from 1 to 5 star
 var comments= prompt("Any additional comments would be greatly appreciated:"); // variable prompt user input additional comments
-var valueInput= alert("Thank you for your feedback! \nYou have just earned 25% Off discount! \nYour coupon code is '('MYSTAR2013')");
+var valueInput= alert("Thank you for your feedback! \nYou have just earned 25% Off discount! \nYour coupon code is '('MYSTAR2013')"); //variable for coupon code earned
