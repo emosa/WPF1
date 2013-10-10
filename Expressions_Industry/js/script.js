44ae23593a93753fc64a202379c28ef4ddbe5d58
Elimarie <iglitzi@fullsail.edu>
2013-10-10 19:42:04 -0400

    fixed comment tittle

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,7 @@
 // October 10, 2013
 // Assignment Expressions
 
-// Rate us and earn a discount
+// Rate us and earn a discount alert and prompt
 
 var salute= alert("Welcome to My Shop! \nRate us and earn a discount!"); // variable for welcome alert
 var userName= prompt("Please enter your first and last name"); // variable for user input
