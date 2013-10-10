795ea3d1189e189ac9ee613e1b6480a50824c21f
Elimarie <iglitzi@fullsail.edu>
2013-10-09 21:00:50 -0400

    Assignment Expression Personal How much water my fish tank holds?

---
+++ Expressions_Personal/script.js
@@ -0,0 +1,19 @@
+// Elimarie Morales Santiago
+// October 9, 2013
+
+// Assignment: Expressions
+
+// How much water my fish tank holds?
+
+var depthInches= 28; //distance from front to back
+var widhtInches= 14; // Distance across the front
+var heightInches= 18; // Distance from top to bottom
+
+var total= depthInches * widhtInches * heightInches;
+
+var gallonsub= total * 0.00433;
+
+
+var watergallon= "My fish tank holds" +" "+ gallonsub + " " + "gallons of water.";
+
+console.log(watergallon);
