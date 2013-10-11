9e78dde9aa5cf64b38afbcf771d7ae596b8cee80
Elimarie <iglitzi@fullsail.edu>
2013-10-10 22:41:35 -0400

    Trying other code

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -4,25 +4,26 @@
 
 // Get Your Astrological Zodiac Find Your Ruler Planet
 
-var zodiacNames= ["Aries","Leo","Sagittarius","Taurus","Virgo","Capricorn","Gemini","Libra","Aquarius","Cancer","Scorpio","Pisces"];
-var rullerPlanet= ["Mars","Venus","Mercury","Moon","Sun","Mercury","Venus","Pluto","Jupiter","Saturn","Uranus","Neptune"];
-var name = prompt("Enter your Zodiac Name");
+//var zodiacNames= ["Aries","Leo","Sagittarius","Taurus","Virgo","Capricorn","Gemini","Libra","Aquarius","Cancer","Scorpio","Pisces"];
+//var rullerPlanet= ["Mars","Venus","Mercury","Moon","Sun","Mercury","Venus","Pluto","Jupiter","Saturn","Uranus","Neptune"];
+//var name = prompt("Enter your Zodiac Name");
 
 
-aler = zodiacNames[0] = rullerPlanet[0];
-zodiacNames[1] = rullerPlanet[1];
-zodiacNames[2] = rullerPlanet[2];
-zodiacNames[3] = rullerPlanet[3];
-zodiacNames[4] = rullerPlanet[4];
-zodiacNames[5] = rullerPlanet[5];
-zodiacNames[6] = rullerPlanet[6];
-zodiacNames[7] = rullerPlanet[7];
-zodiacNames[8] = rullerPlanet[8];
-zodiacNames[9] = rullerPlanet[9];
-zodiacNames[10] = rullerPlanet[10];
-zodiacNames[11] = rullerPlanet[11];
-zodiacNames[12] = rullerPlanet[12];
+//alert = zodiacNames[0] = rullerPlanet[0];
+//zodiacNames[1] = rullerPlanet[1];
+//zodiacNames[2] = rullerPlanet[2];
+//zodiacNames[3] = rullerPlanet[3];
+//zodiacNames[4] = rullerPlanet[4];
+//zodiacNames[5] = rullerPlanet[5];
+//zodiacNames[6] = rullerPlanet[6];
+//zodiacNames[7] = rullerPlanet[7];
+//zodiacNames[8] = rullerPlanet[8];
+//zodiacNames[9] = rullerPlanet[9];
+//zodiacNames[10] = rullerPlanet[10];
+//zodiacNames[11] = rullerPlanet[11];
+//zodiacNames[12] = rullerPlanet[12];
 
 
 // I couldn't finish the code I have tried everything and it don't work I guess I will learn in the future
 
+// Running out time but I am trying a new code
