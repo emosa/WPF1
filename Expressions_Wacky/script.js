4e6a7189b183cc344b29fbf3d78e8423718d4bb1
Elimarie <iglitzi@fullsail.edu>
2013-10-10 20:41:02 -0400

    change project title again

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -2,6 +2,7 @@
 // October 10, 2013
 // Assignment Expressions
 
-// Get Your Astrological Zodiac
+// Get Your Astrological Zodiac Find Your Ruler Planet
 
 var zodiacNames= ["Aries","Leo","Sagittarius","Taurus","Virgo","Capricorn","Gemini","Libra","Aquarius","Cancer","Scorpio","Pisces"];
+
