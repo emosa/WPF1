e519d68a9a6b083546e6e755fa3aa76b37017c22
Elimarie <iglitzi@fullsail.edu>
2013-10-10 20:38:19 -0400

    change project tittle

--- Expressions_Wacky/script.js
+++ Expressions_Wacky/script.js
@@ -2,6 +2,6 @@
 // October 10, 2013
 // Assignment Expressions
 
-// Get Your Chinese Astrological Sign Name
+// Get Your Astrological Zodiac
 
 var zodiacNames= ["Aries","Leo","Sagittarius","Taurus","Virgo","Capricorn","Gemini","Libra","Aquarius","Cancer","Scorpio","Pisces"];
