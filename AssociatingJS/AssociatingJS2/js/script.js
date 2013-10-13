011ca7025aa7eb5317361cd4993a4d02fa1237db
Elimarie <iglitzi@fullsail.edu>
2013-10-13 12:25:44 -0400

    Manipulating variables

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,18 +1,19 @@
 // Elimarie Morales Santiago
 // October 13, 2013
 
-// Conditional logic
+// Condition with expression
 
 
 
-var kidHeight= 50;
+var kidHeight= 47;
 var miheight = 48;
+var sneakerLift= 2;
 
 // if the child is old, enough, print to console "you can ride!"
 
 // if the kid is over 48 inches height
 
-if(kidHeight > miheight){
+if(kidHeight + sneakerLift > miheight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
