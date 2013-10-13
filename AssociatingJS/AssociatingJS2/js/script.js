6e2e32d3b88b79d15f7ba941166b61f2c4bec8e7
Elimarie <iglitzi@fullsail.edu>
2013-10-13 12:19:02 -0400

    Comparing variable and conditional statements

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -5,11 +5,14 @@
 
 
 
-var oldEnough= false;
+var kidHeight= 50;
+var miheight = 48;
 
 // if the child is old, enough, print to console "you can ride!"
 
-if(oldEnough){
+// if the kid is over 48 inches height
+
+if(kidHeight > miheight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
