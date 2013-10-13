5fe8386c195583129b8f3690e3a66bf8900ce9a5
Elimarie <iglitzi@fullsail.edu>
2013-10-13 12:09:25 -0400

    Conditional logic screencast practice

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,24 +1,16 @@
 // Elimarie Morales Santiago
-// October 9, 2013
-// Screencast: Prompt and Alert
+// October 13, 2013
 
-
-// var userInput= prompt("Enter your year of birth:");
-//console.log(userInput);
-
-var width= prompt("We are calculating the area of a rectangle. \nPlease calculate the width.");
-var height= prompt("Please enter the height");
-
-var area= width*height;
-
-//console.log(area); print out to console
-
-// alert("The are of your rectangle is " + area); //concatenating with area
-
-var result= "The are of your rectangle is " + area;  //another way of doing this
-alert(result);
+// Conditional logic
 
 
 
+var oldEnough= false;
 
+// if the child is old, enough, print to console "you can ride!"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
 
