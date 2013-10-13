72f896ae0cf7c80f5a635b959349e854e259e6cd
Elimarie <iglitzi@fullsail.edu>
2013-10-13 15:59:22 -0400

    screencast ternary form

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,14 +1,18 @@
 // Elimarie Morales Santiago
 // October 13, 2013
 
-// Condition logic -  Logical Operators
+// Conditional Logic - Ternary Operators
 
-var budget = 100;
-var iPhonePrice= 199.99;
-var wonLottery= true;
-// if the price of phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget || wonLottery ){
-    console.log(" We can buy the phone!")
+var gpa = 48;
+// if the gpa is over the min 2.0 score, the student can graudate
+
+/* if(gpa > 2.0){
+    console.log("You can graduate");
 }else{
-    console.log("No Phone for you!!");
-}
+    console.log("GPA is too low!");
+} */
+
+// ternary form
+(gpa > 2.0) ? console.log("You can graduate"): console.log("GPA is too low!");
+
+
