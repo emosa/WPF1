1d5d804c2091987f7b2fc80ec76cba00dde3af29
Elimarie <iglitzi@fullsail.edu>
2013-10-13 12:38:25 -0400

    (else) choosing between to lines of codes

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,13 +1,12 @@
 // Elimarie Morales Santiago
 // October 13, 2013
 
-// Condition logic -  with an expression
+// Condition logic -  with an expression (else)
 
 
 
 var kidHeight= 52;
 var miheight = 48;
-
 
 // if the child is old, enough, print to console "you can ride!"
 
@@ -16,7 +15,7 @@ var miheight = 48;
 if(kidHeight > miheight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
-}
-if(kidHeight <= miheight){
+}else{
+    //code performed if condition is false
     console.log("Sorry kid, you've got some growing to do first! ");
 }
