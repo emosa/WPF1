ed74aad58f4fc8018774f0aeabbacd12de3f56fe
Elimarie <iglitzi@fullsail.edu>
2013-10-13 15:46:15 -0400

    logical operators screencast ||

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -3,11 +3,11 @@
 
 // Condition logic -  Logical Operators
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice= 199.99;
-var paycheck= 200;
+var wonLottery= true;
 // if the price of phone is less our budget AND if our paycheck is over 300
-if(iPhonePrice < budget &&paycheck > 300 ){
+if(iPhonePrice < budget || wonLottery ){
     console.log(" We can buy the phone!")
 }else{
     console.log("No Phone for you!!");
