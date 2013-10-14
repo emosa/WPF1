e7657e22cf1fbb7a03c1e1c185d128b191fe21c2
Elimarie <iglitzi@fullsail.edu>
2013-10-13 21:23:18 -0400

    added operation with conditionlals but still figuring out the problem

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -1,12 +1,20 @@
 // Elimarie Morales Santiago
-// October 13, 2013
+// October 13, 2013(in mpg)s
 
 // Conditionals Worksheet
 
 // Last Chance for Gas!
 
-var carMpg= 28;
+var carGasMpg= 28;
 var gasTankReading= 0.050;
-var gasTankGal= 20;
+var gasTankGallons= 20;
+
+
+if(carGasMpg * gasTankReading > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else if (carGasMpg * gasTankReading < 200){
+    console.log("You only have" + " " + gasTankGallons + " "+ "gallons of gas in your tank, better get gas now while you can!");
+}
+
 
 
