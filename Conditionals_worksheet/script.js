c1e5dc289e1eb4c0906266ccbca25d7e9aeebe5a
Elimarie <iglitzi@fullsail.edu>
2013-10-13 22:03:53 -0400

    problem solved added total gallons left and comments

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -8,11 +8,13 @@
 var carGasMpg= 28; // Gas miles per gallon
 var gasTankReading= 0.050;// Half tank
 var gasTankGallons= 20;// Gas tank gallon capacity
+var totalGallonLeft= gasTankGallons / 2; // Gallons capacity divided by half gallon
+
 
 if(carGasMpg * gasTankReading > 200){ // if the gas per gallon multiplied by the tank reading (half tank) is greater than 200 miles
     console.log("Yes, you can make it without stopping for gas!"); // print out console
 }else if (carGasMpg * gasTankReading < 200){ // if the gas per gallon multiplied by the tank reading (half tank) is less than 200 miles
-    console.log("You only have" + " " + gasTankGallons + " "+ "gallons of gas in your tank, better get gas now while you can!"); // print out console
+    console.log("You only have" + " " + totalGallonLeft + " "+ "gallons of gas in your tank, better get gas now while you can!"); // print out console
 }
 
 
