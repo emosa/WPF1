944644cdbf6c937234504727ea5d0ae07797c0dc
Elimarie <iglitzi@fullsail.edu>
2013-10-13 21:40:29 -0400

    resolve problem and added comments

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -5,15 +5,14 @@
 
 // Last Chance for Gas!
 
-var carGasMpg= 28;
-var gasTankReading= 0.050;
-var gasTankGallons= 20;
+var carGasMpg= 28; // Gas miles per gallon
+var gasTankReading= 0.050;// Half tank
+var gasTankGallons= 20;// Gas tank gallon capacity
 
-
-if(carGasMpg * gasTankReading > 200){
-    console.log("Yes, you can make it without stopping for gas!");
-}else if (carGasMpg * gasTankReading < 200){
-    console.log("You only have" + " " + gasTankGallons + " "+ "gallons of gas in your tank, better get gas now while you can!");
+if(carGasMpg * gasTankReading > 200){ // if the gas per gallon multiplied by the tank reading (half tank) is greater than 200 miles
+    console.log("Yes, you can make it without stopping for gas!"); // print out console
+}else if (carGasMpg * gasTankReading < 200){ // if the gas per gallon multiplied by the tank reading (half tank) is less than 200 miles
+    console.log("You only have" + " " + gasTankGallons + " "+ "gallons of gas in your tank, better get gas now while you can!"); // print out console
 }
 
 
