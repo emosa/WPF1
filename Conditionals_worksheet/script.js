08bc637e6ae57510831d19abb5c8c0096e90ea1a
Elimarie <iglitzi@fullsail.edu>
2013-10-14 21:19:52 -0400

    assignment check the login

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -1,21 +1,9 @@
 // Elimarie Morales Santiago
-// October 13, 2013(in mpg)s
+// October 14, 2013(in mpg)s
 
 // Conditionals Worksheet
 
-// Last Chance for Gas!
-
-var carGasMpg= 28;// Gas miles per gallon
-var gasTankReading= 0.050;// Half tank
-var gasTankGallons= 20;// Gas tank gallon capacity
-var totalGallonLeft= gasTankGallons / 2; // Gallons capacity divided by half gallon
-
-
-if(carGasMpg * gasTankReading > 200){ // if the gas per gallon multiplied by the tank reading (half tank) is greater than 200 miles
-    console.log("Yes, you can make it without stopping for gas!"); // print out console
-}else if (carGasMpg * gasTankReading < 200){ // if the gas per gallon multiplied by the tank reading (half tank) is less than 200 miles
-    console.log("You only have" + " " + totalGallonLeft + " "+ "gallons of gas in your tank, better get gas now while you can!"); // print out console
-}
+// Check the Login
 
 
 
