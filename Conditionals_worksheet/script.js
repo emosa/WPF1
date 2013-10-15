c420f87687b13d45949030bd6dccc0a2ee7eaacf
Elimarie <iglitzi@fullsail.edu>
2013-10-14 21:36:28 -0400

    creating variables and testing codes

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -6,4 +6,18 @@
 // Check the Login
 
 
+var userName= "John Doe";
+var password= "Eli@10459";
+
+prompt("Welcome Enter your user name");
+if (userName == userName){
+    prompt(Please enter your password)
+}
+} else if {
+    (userName != userName)
+prompt()
+
+}
+
+
 
