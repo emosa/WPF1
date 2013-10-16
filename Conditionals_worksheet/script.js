13608d6688d923d2bdd8e53192ae68d385558b7b
Elimarie <iglitzi@fullsail.edu>
2013-10-15 21:37:59 -0400

    Conditionals worksheet  Tire Pressure I

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -3,30 +3,8 @@
 
 // Conditionals Worksheet
 
-// Check the Login
+// Tire Pressure I
 
-var uName= "J"; // user name input
-var passW= "1"; // user input password
-var userName= prompt("Enter username");
-var passInput= prompt("Please enter your password");
-
-if (uName == userName)
-    { //if this condition is true
-    alert("Welcome"+" "+  userName +"!");
-
-    if (passInput == passW)
-        {
-        alert("Password does match our record");  // if user input match
-        }
-    else {
-        alert("Password does not match our record");
-         // alert if password don't match
-        }
-    }
-else{
-    alert("User not found. Try again.");
-    // if this condition is false alert user
-    }
 
 
 
