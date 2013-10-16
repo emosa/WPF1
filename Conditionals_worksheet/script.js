073844575845f0b0e62e0cf594c76c4371c36a49
Elimarie <iglitzi@fullsail.edu>
2013-10-15 20:07:37 -0400

    moving and changing code arround

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -5,35 +5,19 @@
 
 // Check the Login
 
-
-var uName="John Doe";
-var pasw= "Eli@10459";
+var uName= "John Doe"; // user name input
+var passW= "12345";
 var userName= prompt("Enter username");
-var password= prompt("Enter your password");
+var passInput= prompt("Please enter your password");
 
-
-if (userName != uName){ //if this condition is false
-    alert("User not found. Try again."); // alert user
-
-}else if (uName != pasw){
-    alert("Password does not match our record");
-}else{
+if (uName === userName){ //if this condition is true
     alert("Welcome"+" "+  userName +"!");
+}else{
+    alert("User not found. Try again."); // if this condition is false alert user
+}
+if (uName != passW){
+    alert("Password does not match our record");
 }
 
 
-//alert("User not found. Try again.");
-//} else if( password != password){
-//    alert("Password does not match our record");
-//} else {(userName == password)
-//
 
-//(userName != password)
-
-//prompt("Enter username");
-
-//prompt("Please enter your password");
-//if (password != userName){
- //   alert("Password does not match our record");
-
-//prompt("Please enter your password");
