fc2c264d3e44ec118aee76529afc59388219c48e
Elimarie <iglitzi@fullsail.edu>
2013-10-15 23:41:14 -0400

    movie ticket price assignment

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -3,23 +3,23 @@
 
 // Conditionals Worksheet
 
-// Tire Pressure I
+// Movie ticket price
 
-var tire=(4); //array for tires
-tire[0]= 38; // tire  psi pressure
-tire[1]= 37;  // tire  psi pressure
-tire[2]= 38;   // tire  psi pressure
-tire[3]= 38;     // tire  psi pressure
 
-var psi = 38; // normal tire psi pressure
 
-if (psi >= tire){// if it is true
-    console.log("The tires pass spec"); //print out console
-}
-else // if it is not true
-{
-    console.log("Get your tires checked out"); // print out console
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
