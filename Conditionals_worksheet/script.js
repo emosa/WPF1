f4a2bf62aee7b2d0b977bd44cbbe9731c72ccfa4
Elimarie <iglitzi@fullsail.edu>
2013-10-15 23:37:08 -0400

    conditionals assigment added conditionals fixed arrays solve problem

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -5,20 +5,20 @@
 
 // Tire Pressure I
 
-var tirePressure =["tire1","tire2", "tire3", "tire4"]; //array for tires
-tire1= "38"; // tire  psi pressure
-tire2= "38";
-tire3= "38";
-tire4= "38";
+var tire=(4); //array for tires
+tire[0]= 38; // tire  psi pressure
+tire[1]= 37;  // tire  psi pressure
+tire[2]= 38;   // tire  psi pressure
+tire[3]= 38;     // tire  psi pressure
 
-var psi="38";
+var psi = 38; // normal tire psi pressure
 
-if (psi < tirePressure){
-    console.log("The tires pass spec");
+if (psi <= tire){// if it is true
+    console.log("The tires pass spec"); //print out console
 }
-else
+else // if it is not true
 {
-    console.log("Get your tires checked out");
+    console.log("Get your tires checked out"); // print out console
 }
 
 
