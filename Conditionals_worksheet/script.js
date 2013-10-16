a9d89e96a816fb79b5bd14af76e81d6556e522ea
Elimarie <iglitzi@fullsail.edu>
2013-10-15 22:14:00 -0400

    added comments psi pressure tire

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -5,15 +5,15 @@
 
 // Tire Pressure I
 
-var tirePressure =["tire1","tire2", "tire3", "tire4"];
-tire1= "38";
+var tirePressure =["tire1","tire2", "tire3", "tire4"]; //array for tires
+tire1= "38"; // tire  psi pressure
 tire2= "38";
 tire3= "38";
 tire4= "38";
 
 var psi="38";
 
-if (psi <= tirePressure){
+if (psi < tirePressure){
     console.log("The tires pass spec");
 }
 else
