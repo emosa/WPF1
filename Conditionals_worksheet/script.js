cb03dd6425ac1c8a244966baec54837bac8b684d
Elimarie <iglitzi@fullsail.edu>
2013-10-16 00:02:39 -0400

    added console log and variables

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -6,11 +6,18 @@
 // Movie ticket price
 
 
+var ticketRegPrice= 12.00;
+var ticketSenior= 7.00;
+var ageSenior= 55;
+var afternoon= 3;
+var afternoon2= 5;
+
+
+if (
 
 
 
-
-
+   console.log("The ticket price is"+ ");
 
 
 
