04c60022ac499eb9fdc15e33b641d60975e47e0d
Elimarie <iglitzi@fullsail.edu>
2013-10-15 22:00:27 -0400

    added conditonals all working

--- Conditionals_worksheet/script.js
+++ Conditionals_worksheet/script.js
@@ -6,10 +6,14 @@
 // Tire Pressure I
 
 var tirePressure =["tire1","tire2", "tire3", "tire4"];
+tire1= "38";
+tire2= "38";
+tire3= "38";
+tire4= "38";
 
 var psi="38";
 
-if (psi == 0 ){
+if (psi <= tirePressure){
     console.log("The tires pass spec");
 }
 else
