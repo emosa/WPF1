a119bc925be1c22e4e2ba2a6c3f30ed36ea5b84c
Elimarie <iglitzi@fullsail.edu>
2013-10-16 21:16:59 -0400

    alert if right

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -7,8 +7,8 @@
 // How many 20 dollars bills are in 500 dollars?
 
 
-var question= prompt("How many dollars bills are in 500 dollars");
+var question= prompt("How wise are you? How many dollars bills are in 500 dollars");
 
 if (question== 25) {
-
+   alert("Wohoo! you are money wise")
 }
