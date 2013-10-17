f1e167df1227da5763e977838dd328b46f2796c6
Elimarie <iglitzi@fullsail.edu>
2013-10-17 18:54:41 -0400

    fixed tipos and tested code working

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -8,7 +8,7 @@
 var itemGildanShirts; //variable for shirts
 var shirtStock= prompt("Please check our stock before placing your order. \n How many shirts do you need?"); // prompt message for customer. If the customer needs more than 400 shirts he needs to call to verify stock.
 
-itemGildanShirts= (shirtStock < 400)? "Available in stock. Please proceed with your order": "Call 1-877-571-0000 to verify stock and place your order";
+itemGildanShirts= (shirtStock < 400)? "Available in stock. Please proceed with your order": "Call 1-877-571-0000 to verify stock and to place your order.";
 
 //conditionals with ternaries
 
