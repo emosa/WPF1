8721d7a0f799d905f91872bc8825fd23c94128b9
Elimarie <iglitzi@fullsail.edu>
2013-10-17 19:40:02 -0400

    added comments and tested code working

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -9,12 +9,13 @@ var gucciJacket= 1600; // cost of jacket
 var bankSavings= 1500; // bank savings
 var cashMoney= 1400; // cash money in pockets
 
-if (gucciJacket >= bankSavings){
-    alert("Enjoy your new jacket!");
+
+if (gucciJacket <= bankSavings){
+    alert("Enjoy your new jacket!"); // if the jacket is cost is greater than or equal to bank savings
 }
 else if (cashMoney < gucciJacket){
-    alert("Choose a cheaper jacket!");
+    alert("Choose a cheaper jacket!");// if cash is less than jacket cost, can choose a cheaper jacket
 }
 else{
-    alert("Sorry maybe another time");
+    alert("Sorry maybe another time"); // if it doesn't have enough money another time will be
 }
