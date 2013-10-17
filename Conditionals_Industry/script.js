c457770f4a1040fc9fd7fabad73265362c282671
Elimarie <iglitzi@fullsail.edu>
2013-10-17 18:46:14 -0400

    added variable prompt with comments

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -6,3 +6,4 @@
 // Check item stock inventory before purchasing
 
 var itemGildanShirts; //variable for shirts
+var shirtStock= prompt("Please check our stock before placing your order. \n How many shirts do you need?"); // prompt message for customer
