578f55e94bf41b063ee34a2420a555ca48dc35d0
Elimarie <iglitzi@fullsail.edu>
2013-10-20 17:40:45 -0400

    screencast Functions - Basic Structure

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -1,29 +1,15 @@
 // Elimarie Morales Santiago
 // October 20, 2013
 
-//infinite loops
+// Functions - Basic Structure
 
- var b = 10; // setups the index
-
-while ( b > 0) {// checks the condition
-    console.log(b +' kegs on the wall' );
-    b--; //increments or decrements the index
+function outptMsg(){
+    console.log("Hello World");
 }
 
-console.log('------ Do While Loops ---------');
-
-
-var c = 10;
-
-do {
-    console.log(c + ' kegs on the wall');
-    c--;
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
 }
-while (c > 0);
-
-console.log('------ Do While Loops ---------');
-
-for (var i = 10; i > 0; i--){
-    console.log (i + ' kegs on the wall')
-}
-
