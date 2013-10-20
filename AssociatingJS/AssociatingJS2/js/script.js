1378332f6cf98264848f5c6841c483d19a2478d1
Elimarie <iglitzi@fullsail.edu>
2013-10-20 17:21:52 -0400

    loops screencast

--- AssociatingJS/AssociatingJS2/js/script.js
+++ AssociatingJS/AssociatingJS2/js/script.js
@@ -9,3 +9,21 @@ while ( b > 0) {// checks the condition
     console.log(b +' kegs on the wall' );
     b--; //increments or decrements the index
 }
+
+console.log('------ Do While Loops ---------');
+
+
+var c = 10;
+
+do {
+    console.log(c + ' kegs on the wall');
+    c--;
+}
+while (c > 0);
+
+console.log('------ Do While Loops ---------');
+
+for (var i = 10; i > 0; i--){
+    console.log (i + ' kegs on the wall')
+}
+
