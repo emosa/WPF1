80290cc8bbc7d23b9120693c3cf3c16521f4fcbe
Elimarie <iglitzi@fullsail.edu>
2013-10-22 20:48:26 -0400

    functions inside code with comments

--- Functions_worksheet/script.js
+++ Functions_worksheet/script.js
@@ -6,4 +6,7 @@
 // Circumference
 
 var circle= piCircle (3.14,4);// the value of pi and circumference of circle
-function piCircle(pi,c){ // function for the two values in the variable 
+function piCircle(pi,c){ // function for the two values in the variable
+    var circum = pi*c; // code inside the function value of pi multiplied by circumference of circle
+    return circum; // function giving the info
+}
