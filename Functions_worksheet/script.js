f855c04259296c86ffb3861282396a053b708edf
Elimarie <iglitzi@fullsail.edu>
2013-10-22 21:09:18 -0400

    stings variable for sting and pound with comments

--- Functions_worksheet/script.js
+++ Functions_worksheet/script.js
@@ -5,8 +5,13 @@
 
 // Circumference
 
-var circle= piCircle (3.14,4);// the value of pi and circumference of circle
-function piCircle(pi,c){ // function for the two values in the variable
-    var circum = pi*c; // code inside the function value of pi multiplied by circumference of circle
-    return circum; // function giving the info
-}
+//var circle= piCircle (3.14,4);// the value of pi and circumference of circle
+//function piCircle(pi,c){ // function for the two values in the variable
+//    var circum = pi*c; // code inside the function value of pi multiplied by circumference of circle
+//    return circum; // function giving the info
+//}
+//console.log("The circumference of the circle is" + " " + circle); // print to console circumference of circle
+
+// Stung!
+
+var victim= stings (8.666666667,1);// variable for stings and pounds
