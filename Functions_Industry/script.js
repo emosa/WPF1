e337f65a5c27e589b7586c3470b470239077f63b
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:35:25 -0400

    added comment for alert if qualified

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -9,7 +9,7 @@ var customerCredit = function (monthlyIncome){// function with parameter
      var customerName = prompt("Please enter your name");//prompt for user name
        var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
     if (monthlyIncome >=1600){// if the customer monthly income is greater than or equal to
-        alert(customerName +" "+ "you qualify for a line of credit");
+        alert(customerName +" "+ "you qualify for a line of credit");// if the customer is qualified for credit 
     }
     else{
         alert("Sorry" +" "+ customerName + " "+ " you do not qualify for a line of credit");
