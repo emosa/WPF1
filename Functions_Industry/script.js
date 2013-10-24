7ac89ce226a5bcbe421cb9f48b84371dbedb7e7a
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:56:54 -0400

    fixed sentences in alerts

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -7,12 +7,12 @@
 
 var customerCredit = function (creditLine){// function with parameter
      var customerName = prompt("Please enter your name");//prompt for user name
-       var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
+       var monthSalary = prompt("Please enter your Net monthly income:");// prompt for customer salary
     if (creditLine <= monthSalary){// if the customer monthly income is greater than or equal to
-        alert(customerName +" "+ "you qualify for a line of credit");// if the customer is qualified for credit
+        alert(customerName +" "+ "you qualify for a line of credit!");// if the customer is qualified for credit
     }
     else{
-        alert("Sorry" +" "+ customerName + " "+"you do not qualify for a line of credit");// if the customer is not qualified
+        alert("Sorry" +" "+ customerName + " "+"you do not qualify for a line of credit.");// if the customer is not qualified
     }
 };
 customerCredit(1200);// call function
