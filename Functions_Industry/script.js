fd0c58894c82fe3c143fd35ffa0ff129204bc10c
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:25:01 -0400

    variable prompt for customer input and comments

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -6,5 +6,5 @@
 // Store credit check
 
 var customerCredit = function (monthlyIncome){// function with parameter
-
+       var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
 }
