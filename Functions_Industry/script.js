8845cabb3b6b22b4a79f2e1da6e7047f3c946b6a
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:29:40 -0400

    added variable prompt for customer name with comments and if statement

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -6,5 +6,9 @@
 // Store credit check
 
 var customerCredit = function (monthlyIncome){// function with parameter
+     var customerName = prompt("Please enter your name");//prompt for user name
        var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
+    if (monthlyIncome >=1600){
+        alert(customerName +" "+ "you qualify for a line of credit");
+    }
 }
