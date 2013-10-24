ed8f123c9e8ac381a200b2b1a2b14b9025dc9b54
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:54:38 -0400

    fixed if statements and function parameter and value code working yey!!!

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -5,14 +5,15 @@
 
 // Store credit check
 
-var customerCredit = function (monthlyIncome){// function with parameter
+var customerCredit = function (creditLine){// function with parameter
      var customerName = prompt("Please enter your name");//prompt for user name
        var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
-    if (monthlyIncome <=1200){// if the customer monthly income is greater than or equal to
+    if (creditLine <= monthSalary){// if the customer monthly income is greater than or equal to
         alert(customerName +" "+ "you qualify for a line of credit");// if the customer is qualified for credit
     }
     else{
-        alert("Sorry" +" "+ customerName + " "+ " you do not qualify for a line of credit");// if the customer is not qualified
+        alert("Sorry" +" "+ customerName + " "+"you do not qualify for a line of credit");// if the customer is not qualified
     }
 };
-customerCredit(2000);// call function
+customerCredit(1200);// call function
+
