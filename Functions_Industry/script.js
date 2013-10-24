af25772262029c364569c9c6ea2ee428d05c14e9
Elimarie <iglitzi@fullsail.edu>
2013-10-23 21:36:24 -0400

    added comment for else if customer is not qualified and comment to call function

--- Functions_Industry/script.js
+++ Functions_Industry/script.js
@@ -9,10 +9,10 @@ var customerCredit = function (monthlyIncome){// function with parameter
      var customerName = prompt("Please enter your name");//prompt for user name
        var monthSalary = prompt("Please enter your Net monthly income");// prompt for customer salary
     if (monthlyIncome >=1600){// if the customer monthly income is greater than or equal to
-        alert(customerName +" "+ "you qualify for a line of credit");// if the customer is qualified for credit 
+        alert(customerName +" "+ "you qualify for a line of credit");// if the customer is qualified for credit
     }
     else{
-        alert("Sorry" +" "+ customerName + " "+ " you do not qualify for a line of credit");
+        alert("Sorry" +" "+ customerName + " "+ " you do not qualify for a line of credit");// if the customer is not qualified
     }
 };
-customerCredit(2000);
+customerCredit(2000);// call function
