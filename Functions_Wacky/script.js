71ef76262aa9c2fdbe77f1bfc91103fb5600056a
Elimarie <iglitzi@fullsail.edu>
2013-10-24 21:29:31 -0400

    added alert with parameters

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -6,3 +6,4 @@
 // function with more than one parameter
 
 function savings(ten, twenty, thirty){// function with parameters
+    alert("If I make the effort and save some" + " " + ten +" "+ "by December I can finally" + " " + twenty + " "+ "my new" +" "+ thirty);
