cc7f1819698cf46e6f3517d09e3618bbf352504e
Elimarie <iglitzi@fullsail.edu>
2013-10-24 22:09:57 -0400

    added another exersice honestly I couldn't figure out how to put the functions together with the loops.

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -10,3 +10,13 @@ function savings(ten, twenty, thirty){// function with parameters
 }
 
 savings("money", "buy", "car!");// called parameters
+
+
+// added another exersice honestly I couldn't figure out how to put the functions together with the loops.
+
+
+var whatToEat = ["Taco", "Pizza", "Fruits", "Vegetables"];//declares the array has 4 elements
+
+for (var i = 0; i < whatToEat.length; i++) { //loop starts will run until i < 4 and it will stop once i is no longer less than what to eat
+    console.log("I would like to eat " + whatToEat[i]);// print out what to eat because
+}
